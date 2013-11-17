71443cd53944c6fa5e85bc3b8faa53b21d74cf96
Chau Truong Thinh <chautruongthinh@ChauTien-PC.(none)>
2013-11-18 01:09:30 +0700

    android_device_htc_leo

---
+++ BoardConfig.mk
@@ -0,0 +1,79 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+#
+
+# WARNING: This line must come *before* including the proprietary
+# variant, so that it gets overwritten by the parent (which goes
+# against the traditional rules of inheritance).
+USE_CAMERA_STUB := true
+
+# inherit from the proprietary version
+-include vendor/htc/leo/BoardConfigVendor.mk
+# inherit common defines for all qsd8k devices
+include device/htc/qsd8k-common/BoardConfigCommon.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := htcleo
+
+BOARD_KERNEL_CMDLINE := no_console_suspend=1 wire.search_count=5
+BOARD_KERNEL_BASE := 0x11800000
+BOARD_KERNEL_NEW_PPPOX  := true
+
+# Graphics
+TARGET_USES_MDP3 := true
+TARGET_USES_PMEM := true
+BOARD_ADRENO_AVOID_EXTERNAL_TEXTURE := true
+COMMON_GLOBAL_CFLAGS    += -DREFRESH_RATE=60
+
+# Kernel directory
+TARGET_KERNEL_CONFIG    := htcleo_defconfig
+TARGET_PREBUILT_KERNEL  := device/htc/leo/prebuilt/kernel
+
+# to enable the GPS HAL
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := htcleo
+
+
+# AMSS version to use for GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
+
+
+# Pull all dictionaries
+TARGET_USE_KEYBOARD := international
+
+# Use legacy touchscreen
+BOARD_USE_LEGACY_TOUCHSCREEN := true
+
+# Ril
+LEGACY_RIL := true
+BOARD_USES_LEGACY_RIL := true
+
+# Hacks
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
+BOARD_USE_LEGACY_TRACKPAD := true
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x9C4B40
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00600000
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 9999999999
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x0c800000
+BOARD_FLASH_BLOCK_SIZE := 131072
+
+TARGET_RECOVERY_FSTAB = device/htc/leo/ramdisk/fstab.htcleo
+RECOVERY_FSTAB_VERSION := 2
+
