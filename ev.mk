802429e95d6fb96295455331291a55c59213dcf8
C457 <android.c357@gmail.com>
2013-12-01 22:37:01 -0600

    Evervolv has the best support

--- ev.mk
+++ ev.mk
@@ -4,16 +4,19 @@ PRODUCT_RELEASE_NAME := Leo
 # Inherit device configuration for leo.
 $(call inherit-product, device/htc/leo/full_leo.mk)
 
-# Inherit some common cm stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common evervolv stuff.
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_small_phone.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_htcleo
+PRODUCT_NAME := ev_htcleo
 PRODUCT_DEVICE := htcleo
 
-# Bootanimation
-TARGET_SCREEN_HEIGHT := 800
-TARGET_SCREEN_WIDTH := 480
+# Hot reboot
+PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
+
+# Copy compatible prebuilt files
+PRODUCT_COPY_FILES +=  \
+    vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_FINGERPRINT=google/htc_leo/leo:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="leo-user 4.2.2 JDQ39 573038 release-keys"
